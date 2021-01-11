9dba4bd271aaae5902fe43a5176d052f3bc79432
Eric Karschner <erickarschner@gmail.com>
2021-01-11 17:38:14 -0500

    Add link to QR code

--- src/components/board/Console.js
+++ src/components/board/Console.js
@@ -5,9 +5,11 @@ const Console = (props) => {
   return (
     <div className="center console">
       <button type="button" class="btn btn-primary" onClick={props.beginDoubleJeopardy}>Begin Double Jeopardy</button>
-      <div className="center">
-        <QRCode includeMargin={true} className="center" value={`https://${window.location.host}/play`} />
-      </div>
+      <a href={`https://${window.location.host}/play`}>
+        <div className="center">
+          <QRCode includeMargin={true} className="center" value={`https://${window.location.host}/play`} />
+        </div>
+      </a>
       <p className="normal font-weight-light">Scan to join!</p>
     </div>
   )
