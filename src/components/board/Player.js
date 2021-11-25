d38aac827dc942262cfa68825cc046bae6e1c09a
Eric Karschner <erickarschner@gmail.com>
2021-11-24 23:26:50 -0500

    Allow board to remove players

--- src/components/board/Player.js
+++ src/components/board/Player.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const Player = (props) => {
   return (
     <tr>
       <td className="player">{props.name}</td>
       <td className="player">{props.balance}</td>
+      <Button onClick={() => props.remove(props.name)} variant="danger">Remove</Button>
     </tr>
   );
 };
--- src/components/board/PlayerDisplay.js
+++ src/components/board/PlayerDisplay.js
@@ -2,6 +2,15 @@ import React from "react";
 import Player from "./Player";
 
 const PlayerDisplay = (props) => {
+  const removePlayer = (name) => {
+    const data = {
+      request: "remove",
+      name: name,
+    };
+
+    props.socket.send(JSON.stringify(data));
+  };
+
   return (
     <table id="player">
       <tr>
@@ -11,7 +20,7 @@ const PlayerDisplay = (props) => {
       {props.players
         .sort((a, b) => a.balance - b.balance)
         .map((player) => (
-          <Player key ={player.name} name={player.name} balance={player.balance} />
+          <Player key={player.name} remove={removePlayer} name={player.name} balance={player.balance} />
         ))}
     </table>
   );
